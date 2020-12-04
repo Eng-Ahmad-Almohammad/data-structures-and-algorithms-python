f1832fb4cae5fa3ccd9edcafd20d51ae24a4a12a
Ahmad <almohammedahmed2@gmail.com>
2020-12-04 19:29:15 +0200

    challenge05 had solved

--- data_structures_and_algorithms/data_structures/linked_list/linked_list.py
+++ data_structures_and_algorithms/data_structures/linked_list/linked_list.py
@@ -1,7 +1,66 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+
+
+
+
 class LinkedList:
     """
-    Put docstring here
+    This class will insert a value to linked list in the head by calling insert method
+    and it will search about any value if it exsist in the linked list by calling includes method
+    and it has a __str__ method return a string with all linde list content.
     """
 
     # put your LinkedList implementation here
-    pass
+    def __init__(self):
+        self.head = None
+
+
+    def insert(self,value):
+        if value == None or value == '':
+            raise Exception('You should insert a value')
+
+        node = Node(value)
+        if self.head == None:
+                self.head = node
+        else:
+            node.next=self.head
+            self.head=node
+
+        
+    def includes(self,value):
+        
+        if self.head==None:
+            return False
+        else:
+            current = self.head
+            while current:
+                
+                if current.value == value:
+                    return True
+                current = current.next
+
+            return False
+
+    def __str__(self):
+        string = ''
+        current = self.head
+        while current:
+            string += f'{{{current.value}}}->'
+            current= current.next
+            if current == None:
+                string += 'NULL'
+
+        return (string)
+
+            
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.insert(None)
+    ll.insert(7)
+    print(ll.includes(7))
+    print(ll)
--- tests/data_structures/test_linked_list.py
+++ tests/data_structures/test_linked_list.py
@@ -1,8 +1,35 @@
-from data_structures_and_algorithms.data_structures.linked_list.linked_list import (
-    LinkedList,
-)
+from data_structures_and_algorithms.data_structures.linked_list.linked_list import LinkedList
 
+import pytest
 
 def test_instance():
     ll = LinkedList()
     assert isinstance(ll, LinkedList)
+
+
+
+def test_insert(prep_tt):
+    # head - Node(7) -> Node(5) -> None
+    assert prep_tt.head.value == 7
+    assert prep_tt.head.next.next == None
+    assert prep_tt.head.next.value == 5
+
+
+
+def test_includes(prep_tt):
+    assert prep_tt.includes(7)==True
+    assert prep_tt.includes(-5)==False
+    assert prep_tt.includes('')==False
+
+   
+def test_str(prep_tt):
+    assert str(prep_tt) == f'{{7}}->{{5}}->NULL'
+
+
+
+@pytest.fixture
+def prep_tt():
+    tt = LinkedList()
+    tt.insert(5)
+    tt.insert(7)
+    return tt
